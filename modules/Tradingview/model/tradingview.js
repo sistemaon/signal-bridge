4881cb3874d0494aec2006c76fce34236b956ea2
ltp431 <erni.augusto@gmail.com>
2023-03-31 18:41:46 +0100

    module add
    module trading view add
    model collection skeleton add

---
+++ modules/Tradingview/model/tradingview.js
@@ -0,0 +1,14 @@
+{
+    strategyName: 'Diff Delta' // Name of strategy designed in Tradinview
+    pair: 'ETH/USDT'
+    chartTimeframe: '45min'
+    side: 'LONG' // long or short (buy or sell)
+    entry: '1800' // current price of the signal asset
+    targets: {
+        t1: '1900'
+        t2: '2000'
+        t3: '2500'
+        t4: '3000'
+    }
+    stop: '1500'
+};
