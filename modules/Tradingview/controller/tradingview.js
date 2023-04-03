43d5c7952567a997b37f3a86263b95544225dd69
ltp431 <erni.augusto@gmail.com>
2023-04-03 17:50:17 +0100

    create signal add

--- modules/Tradingview/controller/tradingview.js
+++ modules/Tradingview/controller/tradingview.js
@@ -1,7 +1,32 @@
 
 const tradingview = require('../model/tradingview');
 
-const createSignal = async (req, res) => {};
+const createSignal = async (req, res, next) => {
+
+    try {
+        const { strategyName, pair, chartTimeframe, side, entry, targets, stop, signalTradeType } = req.body;
+
+        const newSignal = new tradingview({
+            strategyName,
+            pair,
+            chartTimeframe,
+            side,
+            entry,
+            targets,
+            stop,
+            signalTradeType
+        });
+
+        const savedSignal = await newSignal.save();
+
+        return res.status(201).json({ savedSignal });
+
+    } catch (error) {
+        console.log("🚀 ~ file: tradingview.js: ~ createSignal ~ error:", error);
+        return res.status(500).json({ error: error });
+    }
+
+};
 
 const tradingviewController = {
     createSignal
