19a1951e8ff94eb9c9d85019f4278e976e3ea61b
ltp431 <erni.augusto@gmail.com>
2023-04-04 18:24:08 +0100

    user controller add user create add

---
+++ modules/User/controller/user.js
@@ -0,0 +1,30 @@
+
+const user = require('../model/user');
+
+const createUser = async (req, res, next) => {
+    try {
+        const { username, exchange } = req.body;
+        if (!username || !exchange) {
+            return res.status(400).json({ message: 'Missing required fields, please check again.' });
+        }
+        
+        const newUser = new user({
+            username,
+            exchange
+        });
+
+        const user = await newUser.save();
+
+        return res.status(201).json(user);
+
+    } catch (error) {
+        console.log("🚀 ~ file: user.js:9 ~ createUser ~ error:", error);
+        return res.status(500).json({ error: error });
+    }
+};
+
+const userController = {
+    createUser
+};
+
+module.exports = userController;
