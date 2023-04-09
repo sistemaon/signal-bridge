470e01491ef6b165cd3b4154c632d3a4bb910271
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-04-09 21:30:27 +0100

    user model add unique attributes option

--- modules/User/model/user.js
+++ modules/User/model/user.js
@@ -7,17 +7,20 @@ const conn = require('../../../configs/db.config');
 const userSchema = new Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     exchange: {
         binance: {
             apiKey: {
                 type: String,
-                required: true
+                required: true,
+                unique: true
             },
             apiSecret: {
                 type: String,
-                required: true
+                required: true,
+                unique: true
             }
         }
     }
