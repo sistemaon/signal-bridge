0f39de788cb779decd9fcd98d2776ab6ed7582e4
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-04-16 21:49:42 +0100

    binance add process env options

--- env.example.js
+++ env.example.js
@@ -5,5 +5,10 @@ module.exports = {
     DB_PORT: '27017',
     DB_NAME: 'signal-bridge',
     API_KEY_BINANCE: 'brIngStyOnThERihMBRelick',
-    API_SECRET_BINACE: 'ichArLlisSohnOvNIIHratS'
+    API_SECRET_BINACE: 'ichArLlisSohnOvNIIHratS',
+    TIMEOUT_OPTION_BINANCE: 60000,
+    VERBOSE_OPTION_BINANCE: true,
+    RECONNECT_OPTION_BINANCE: true,
+    ENABLE_RATE_LIMIT_OPTION_BINANCE: true,
+    DEFAULT_TYPE_OPTION_BINANCE: 'future'
 };
