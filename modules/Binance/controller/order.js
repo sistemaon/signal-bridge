e61ae71ae235b8119ccc931fec9b172344e90310
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-04-17 16:58:50 +0100

    binance controller order add fetch user orders

--- modules/Binance/controller/order.js
+++ modules/Binance/controller/order.js
@@ -3,38 +3,54 @@ const Order = require('../model/order');
 
 
 const saveExecutedOrder = async (order, user) => {
-    const saveUserOrder = new Order({
-        info: order.info,
-        id: order.id,
-        clientOrderId: order.clientOrderId,
-        timestamp: order.timestamp,
-        datetime: order.datetime,
-        lastTradeTimestamp: order.lastTradeTimestamp,
-        symbol: order.symbol,
-        type: order.type,
-        timeInForce: order.timeInForce,
-        postOnly: order.postOnly,
-        reduceOnly: order.reduceOnly,
-        side: order.side,
-        price: order.price,
-        triggerPrice: order.triggerPrice,
-        amount: order.amount,
-        cost: order.cost,
-        average: order.average,
-        filled: order.filled,
-        remaining: order.remaining,
-        status: order.status,
-        fee: order.fee,
-        trades: order.trades,
-        fees: order.fees,
-        stopPrice: order.stopPrice,
-        user: user.userId
-    });
-    return await saveUserOrder.save();
+    try {
+        const saveUserOrder = new Order({
+            info: order.info,
+            id: order.id,
+            clientOrderId: order.clientOrderId,
+            timestamp: order.timestamp,
+            datetime: order.datetime,
+            lastTradeTimestamp: order.lastTradeTimestamp,
+            symbol: order.symbol,
+            type: order.type,
+            timeInForce: order.timeInForce,
+            postOnly: order.postOnly,
+            reduceOnly: order.reduceOnly,
+            side: order.side,
+            price: order.price,
+            triggerPrice: order.triggerPrice,
+            amount: order.amount,
+            cost: order.cost,
+            average: order.average,
+            filled: order.filled,
+            remaining: order.remaining,
+            status: order.status,
+            fee: order.fee,
+            trades: order.trades,
+            fees: order.fees,
+            stopPrice: order.stopPrice,
+            user: user.userId
+        });
+        return await saveUserOrder.save();
+    } catch (error) {
+        console.error({ error });
+        return error;
+    }
+};
+
+const fetchUserOrders = async (userId) => {
+    try {
+        const userOrders = await Order.find({ user: userId }).populate('user signal');
+        return userOrders;
+    } catch (error) {
+        console.error({ error });
+        return error;
+    }
 };
 
 const orderController = {
-    saveExecutedOrder
+    saveExecutedOrder,
+    fetchUserOrders
 };
 
 module.exports = orderController;
