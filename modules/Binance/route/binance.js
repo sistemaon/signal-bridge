e4aea577ae0e4251623674a079149450476892f3
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-05-22 14:14:29 +0100

    create route for order target indicator

--- modules/Binance/route/binance.js
+++ modules/Binance/route/binance.js
@@ -7,6 +7,8 @@ const binanceMarket = require('../controller/market');
 
 router.post('/add/order-signal-indicator', binance.createOrderSignalIndicator);
 
+router.post('/add/order-target-indicator', binance.createOrderTargetIndicator);
+
 router.post('/add/markets', binanceMarket.fetchAndSaveMarkets);
 
 router.post('/user/:userId/orders', binance.fetchUserBinanceOrders);
