c24e0fb3463f6b4120a39019976dbf09bb5e73ab
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-06-01 16:27:06 +0100

    add executed user order to add target info relation binance order

--- modules/Binance/controller/order.js
+++ modules/Binance/controller/order.js
@@ -2,7 +2,7 @@
 const Order = require('../model/order');
 
 
-const saveExecutedUserOrder = async (order, user, signal) => {
+const executedUserOrder = async (order, user, signal, targets) => {
     try {
         const saveUserOrder = new Order({
             info: order.info,
@@ -30,7 +30,46 @@ const saveExecutedUserOrder = async (order, user, signal) => {
             fees: order.fees,
             stopPrice: order.stopPrice,
             user: user.userId,
-            signal: signal._id
+            signal: signal._id,
+            targets: targets ? targets : []
+        });
+        return saveUserOrder;
+    } catch (error) {
+        console.error({ error });
+        return error;
+    }
+};
+
+const saveExecutedUserOrder = async (order, user, signal, targets) => {
+    try {
+        const saveUserOrder = new Order({
+            info: order.info,
+            id: order.id,
+            clientOrderId: order.clientOrderId,
+            timestamp: order.timestamp,
+            datetime: order.datetime,
+            lastTradeTimestamp: order.lastTradeTimestamp,
+            symbol: order.symbol,
+            type: order.type,
+            timeInForce: order.timeInForce,
+            postOnly: order.postOnly,
+            reduceOnly: order.reduceOnly,
+            side: order.side,
+            price: order.price,
+            triggerPrice: order.triggerPrice,
+            amount: order.amount,
+            cost: order.cost,
+            average: order.average,
+            filled: order.filled,
+            remaining: order.remaining,
+            status: order.status,
+            fee: order.fee,
+            trades: order.trades,
+            fees: order.fees,
+            stopPrice: order.stopPrice,
+            user: user.userId,
+            signal: signal._id,
+            targets: targets ? targets : []
         });
         return await saveUserOrder.save();
     } catch (error) {
@@ -58,6 +97,7 @@ const fetchUserOrders = async (userId) => {
 };
 
 const orderController = {
+    executedUserOrder,
     saveExecutedUserOrder,
     fetchUserOrders
 };
